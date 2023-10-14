a04991b6eeec2408cf5051c44427449953689a97
HyungCheolSim <sim34122@gmail.com>
2023-10-14 21:54:33 +0900

    :recycle: :refactor Code Convention 에 따라 Import 문 수정 및 * 제거
    Code Convention 에 따라 Import 문 수정 및 * 제거, Readme에 사용되는 이미지 추가

--- src/main/java/com/example/wantedpreonboardingbackend/entity/Company.java
+++ src/main/java/com/example/wantedpreonboardingbackend/entity/Company.java
@@ -1,7 +1,18 @@
 package com.example.wantedpreonboardingbackend.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -12,22 +23,22 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class Company {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "company_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "company_id")
+	private Long id;
 
-    @Column(name = "company_name", nullable = false, length = 32)
-    private String name;
+	@Column(name = "company_name", nullable = false, length = 32)
+	private String name;
 
-    @Column(name = "company_introduction")
-    private String introduction;
+	@Column(name = "company_introduction")
+	private String introduction;
 
-    @Column(name = "nation", nullable = false)
-    private String nation;
-    @Column(name = "region", nullable = false)
-    private String region;
+	@Column(name = "nation", nullable = false)
+	private String nation;
+	@Column(name = "region", nullable = false)
+	private String region;
 
-    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
-    private List<Recruitment> Recruitments;
+	@OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
+	private List<Recruitment> Recruitments;
 }
